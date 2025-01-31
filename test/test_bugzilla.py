907bcde1c95e9aa01b99706ed6bb790b770017df
James Graham <james@hoppipolla.co.uk>
2025-01-31 13:07:06 +0000

    Add some very simple pytest tests
    These just get a real bug from bugzilla to ensure we deserialize correctly

---
+++ test/test_bugzilla.py
@@ -0,0 +1,26 @@
+import pytest
+
+from bugdantic import Bugzilla, BugzillaConfig
+
+
+@pytest.fixture
+def bugzilla():
+    config = BugzillaConfig("https://bugzilla.mozilla.org")
+    return Bugzilla(config)
+
+
+def test_bug_include_default(bugzilla):
+    result = bugzilla.bug(975444)
+    assert result.id == 975444
+    result = bugzilla.bug(975444, include_fields=["_default"])
+    assert result.id == 975444
+
+
+def test_bug_include_all(bugzilla):
+    result = bugzilla.bug(975444, include_fields=["_all"])
+    assert result.id == 975444
+
+
+def test_bug_history_full(bugzilla):
+    result = bugzilla.bug_history(975444)
+    assert result.id == 975444
